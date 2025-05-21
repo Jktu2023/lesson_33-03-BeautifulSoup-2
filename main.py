1d5700d70132089151763a3274f2334a23d8b60c
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-05-22 01:09:51 +0700

    2

--- main.py
+++ main.py
@@ -1,8 +1,60 @@
 from googletrans import Translator
+import requests
+from bs4 import BeautifulSoup
 
-translator = Translator()
-result = translator.translate('hello', dest='ru')
-# text = translator.translate('hello', dest='ja')
-print(result.text)
+
+url = 'https://randomword.com/'
+
+def translate(word):
+    translator = Translator()
+    result = translator.translate(word, dest='ru')
+    # text = translator.translate('hello', dest='ja')
+    return result.text
+
+def get_random_word(): # функция парсер слова и его описания с сайта
+    print(url)
+    try:
+        response = requests.get(url) # получаем ответ
+        if response.ok:
+            print('До сервера достучались. Ok!\n')
+        soup = BeautifulSoup(response.content, 'html.parser') # парсим, создаем обьект суп
+
+        english_word = soup.find('div', id="random_word").text # находим слово в теге с ид
+        print(english_word)
+        word_definition = soup.find('div', id="random_word_definition").text # находим описание слова в теге с ид
+        print(word_definition)
+
+    except Exception as e:
+        print(e)
+
+    return {
+            'english_word' : english_word,
+         'word_definition' : word_definition
+             }
+
+get_random_word()
+
+def game():
+    print('Играем!')
+    while True:
+        word_dict = get_random_word() # парсим слово и описание с сайта и помещаем с словарь
+        english_word = word_dict['english_word'] # значение по ключу из словаря 1м способом
+        english_word_definition = word_dict.get('word_definition')  # значение по ключу из словаря 2м способом
+        russian_word = translate(english_word) # переводим
+        russian_word_definition = translate(english_word_definition) # переводим
+
+        user_say = input(f'Посмотрите описание ({russian_word_definition}) и введите слово которое ему соответствует: - ')
+        if user_say == russian_word:
+            print('Ответ правильный!')
+        else:
+            print(f'Ответ не правильный! Это слово {russian_word}.')
+
+        play_again = input('Попробуете еще раз? y/n')
+        if play_again == 'n':
+            print('Игра сделана!')
+
+
+
+
 
 
