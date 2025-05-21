9ded6382d7445b71756b47563b5ed656999d9662
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-05-22 00:03:09 +0700

    1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print()
+from googletrans import Translator
+
+translator = Translator()
+result = translator.translate('hello', dest='ru')
+# text = translator.translate('hello', dest='ja')
+print(result.text)
+
+
